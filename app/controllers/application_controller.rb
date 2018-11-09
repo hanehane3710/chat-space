1d655df1f993b1bb851b262fbd1cb6424a9ec218
hanehane3710 <hanehane0464@gmail.com>
2018-11-09 10:48:00 +0900

    Devise _strong_parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: :sign_up
+  before_action :devise_params, if: :devise_controller?
+
+  protected
+
+   def devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+   end
+
 end
