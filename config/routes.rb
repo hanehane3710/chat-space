f69a809931478a80ea14829422f762c58de71056
hanehane3710 <hanehane0464@gmail.com>
2018-11-10 13:49:06 +0900

    Routing  groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :messages, only: :index
+  root 'groups#index'
   resources :users, only: [:edit,:update]
+  resources :groups, except: :show do
+    resources :messages, only: [:index,:create]
+  end
 end
