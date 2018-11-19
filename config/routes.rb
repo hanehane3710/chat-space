19792579b7b160f8272e67522441c8f4b9d1e674
hanehane3710 <hanehane0464@gmail.com>
2018-11-19 14:10:28 +0900

    users_edit modify

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   # get 'messages/create'
 
   devise_for :users
-  root 'groups#index'
   resources :users, only: [:edit,:update]
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'groups#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   resources :groups, except: :show do
     resources :messages, only: [:index, :create]
   end
