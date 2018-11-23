01375b5f04cb28e88842931cf76c6eff865f7e44
hanehane3710 <hanehane0464@gmail.com>
2018-11-23 14:50:12 +0900

    Incremental_search modify
    追加ボタンを押した後の処理をajaxでなくjsの操作だけにした

--- app/assets/javascripts/incrimental.js
+++ app/assets/javascripts/incrimental.js
@@ -1,11 +1,12 @@
 $(function(){
-  function buildHTML(user){
-    var html =`<div class="chat-group-user clearfix">
+  function userSearch(user){
+    var html =`<div class="chat-group-user clearfix js-chat-member">
                 <p class="chat-group-user__name">${user.user_name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn">追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="${user.id}" data-user-name="${user.user_name}">追加</a>
                </div>`
-    $('.chat-group-form__search').append(html);
+    $('#user-search-result').append(html);
       }
+
   $('#user-search-field').on('keydown',function(){
     var input = $("#user-search-field").val();
     $.ajax({
@@ -15,12 +16,35 @@ $(function(){
       dataType: 'json'
     })
     .done(function(users){
-      $('.chat-group-form__search').find('.chat-group-user').remove();
+      $('#user-search-result').empty();
       if (users.length !== 0){
         users.forEach(function(user){
-           buildHTML(user);
+           userSearch(user);
         });
-      }
+       }
+        $('#user-search-result').on("click",".chat-group-user__btn--add",function(){
+          var user_id = $(this).data("user-id");
+          var user_name = $(this).data("user-name");
+              console.log(this);
+              console.log(user_id);
+              console.log(user_name);
+          var add_member =`
+                      <div class="chat-group-user js-chat-member clearfix" id="chat-group-user-${user_id}">
+                       <input name="group[user_ids][]" type="hidden" value="${user_id}">
+                       <p class="chat-group-user__name">${user_name}</p>
+                       <a class="chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
+                      </div>`
+
+          $('#chat-group-users').append(add_member);
+          $(this).parent().remove();
+        })
+        $('.chat-group-user').on("click",".chat-group-user__btn--remove",function(){
+          $(this).parent().remove();
+         })
+      })
+    .fail(function(){
+      alert("検索に失敗しました");
     })
-  });
+ });
 });
+
