c53a5ea6531d290084125157275b1050dc1833e8
hanehane3710 <hanehane0464@gmail.com>
2018-11-25 13:02:02 +0900

    自動更新のためのコードをmessage.jsに記述した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,19 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
   function buildHTML(message){
     var image ="";
     if (message.image){
       image = `<br>
-               <p><img src='${message.image.url}'></p>`
+               <p><img src='${message.image.url}' class="image"></p>`
     }
-    var html =`<span class="name">${message.user}</span>
-               <span class="date">${message.created_at}</span>
-               <br>
-               <p>${message.message}</p>
-               ${image}`
+    var html =`<br>
+              <div class="message" data-message-id="${message.id}">
+              <br>
+              <span class="name">${message.user_name}</span>
+              <span class="date">${message.created_at}</span>
+              <br>
+              <p>${message.message}</p>
+              ${image}
+              </div>`
      return html;
   }
   $('#new_message').on('submit',function(e){
@@ -32,4 +36,44 @@ $(function(){
       alert('error');
     });
   });
+  //自動更新
+  function buildHTML(message) {
+  var image = '';
+  if (message.image.url) {
+    image = `<img src="${message.image.url}" class="image">`;
+  }
+  var html = `<br>
+              <div class="message" data-message-id="${message.id}">
+              <br>
+              <span class="name">${message.user_name}</span>
+              <span class="date">${message.created_at}</span>
+              <br>
+              <p>${message.message}</p>
+              ${image}
+              </div>`;
+  return html;
+}
+  var interval = setInterval(function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+    $.ajax({
+      url: location.href.json,
+      dataType: 'json'
+    })
+    .done(function(messages) {
+      var id = $('.message').data('message-id');
+      var html = '';
+      messages.forEach(function(message){
+        console.log(message);
+        if (message.id > id ) {
+          html += buildHTML(message);
+        }
+      });
+      $('.messages__body__message__infomation').prepend(html);
+    })
+    .fail(function(json) {
+      alert('自動更新に失敗しました');
+    });
+  } else {
+    clearInterval(interval);
+   }} , 5 * 1000 );
 });
