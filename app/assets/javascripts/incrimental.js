b3ecc60cb78a1b7ad16ceae5be42b7d9500fb4c1
hanehane3710 <hanehane0464@gmail.com>
2018-11-28 16:44:37 +0900

    Jbuilder json.id create

--- app/assets/javascripts/incrimental.js
+++ app/assets/javascripts/incrimental.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
   function userSearch(user){
     var html =`<div class="chat-group-user clearfix js-chat-member">
                 <p class="chat-group-user__name">${user.user_name}</p>
